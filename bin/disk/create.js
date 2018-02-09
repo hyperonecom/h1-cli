b450e2ce8b42bd0a057979ef49a534a5eaa38375
Frederico Silva <frederico.silva@gmail.com>
2018-02-09 16:18:30 +0100

    disk: read++

--- bin/disk/create.js
+++ bin/disk/create.js
@@ -4,42 +4,31 @@ const Cli = require('structured-cli');
 
 const options = {
     name: {
-        description: 'Disk name',
-        type: 'string',
-        required: true
+        description: 'Disk name'
+      , type: 'string'
+      , required: true
     },
     type: {
-        description: 'Disk type Id',
-        type: 'string',
-        required: true
+        description: 'Disk type Id'
+      , type: 'string'
+      , required: true
     },
     size: {
-        description: 'Disk size in GB',
-        type: 'int',
-        required: true
+        description: 'Disk size in GB'
+      , type: 'int'
+      , required: true
     }
 };
 
-
-module.exports = Cli.createCommand('create', {
-    description: 'Disk create',
-    plugins: [
-        require('../_plugins/loginRequired'),
-        require('../_plugins/tenantRequired'),
-        require('../_plugins/outputFormat'),
-        require('../_plugins/api')
-    ],
-    options: options,
-    handler: handleDiskCreate
+module.exports = resource => Cli.createCommand('create', {
+    description: 'Disk create'
+  , plugins: resource.plugins
+  , options: options
+  , handler: args => args.helpers.api
+        .post(resource.url(args), {
+            name: args.name
+          , service: args.type
+          , size: args.size
+        })
+        .then(result => args.helpers.sendOutput(args, result))
 });
-
-
-function handleDiskCreate(args) {
-
-    return args.helpers.api.post('disk', {
-        name: args.name,
-        service: args.type,
-        size: args.size
-    })
-    .then(result => args.helpers.sendOutput(args, result));
-};
