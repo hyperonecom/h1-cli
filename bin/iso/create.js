1cc19722b3c24dda7b184849c6da84c29716c9e6
Frederico Silva <frederico.silva@gmail.com>
2018-02-13 19:03:52 +0100

    iso create: add metadata, use async/await

--- bin/iso/create.js
+++ bin/iso/create.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Cli = require('structured-cli');
+
+const path = require('path');
 const fs = require('fs');
 
 const websocketStream = require('lib/websocketStream');
@@ -22,36 +24,31 @@ module.exports = resource => Cli.createCommand('create', {
     description: 'ISO import'
   , plugins: resource.plugins
   , options: options
-  , handler: args => {
+  , handler: async args => {
+
+        let iso;
 
         if (args.source.match('^http(s)?://')) {
-            return args.helpers.api
-                .post(resource.url(args), { name: args.name, source: args.source })
-                .then(result => args.helpers.sendOutput(args, result));
+            iso = await args.helpers.api.post(resource.url(args), { name: args.name, source: args.source });
+        } else {
+            const fileSize = fs.statSync(args.source).size;
+
+            iso = await args.helpers.api.post(resource.url(args), {
+                name: args.name
+              , size: fileSize / 1024**3
+              , metadata: {
+                    source: {
+                        filename: path.basename(args.source)
+                      , size: fileSize
+                    }
+                }
+            });
+
+            const ws = await args.helpers.api.wsUpload(`/iso/${iso._id}/upload`);
+
+            await websocketStream.upload(ws, args.source);
         }
 
-        const fileSize = fs.statSync(args.source).size;
-
-        return args.helpers.api
-            .post(resource.url(args), { name: args.name, size: fileSize/1024**3 })
-            .then(result => args.helpers.api
-                .ws(`/iso/${result._id}/upload`)
-                .then(ws => new Promise((resolve, reject) => {
-                    console.log('waiting for the OK to upload');
-                    ws.on('message', data => {
-                        console.log('received', data);
-
-                        if (data !== 'OK') {
-                            return reject(data);
-                        }
-
-                        const stream = websocketStream.upload(ws, args.source);
-                        stream.on('finish', () => resolve(result));
-                    });
-
-                    ws.on('error', reject);
-                }))
-            )
-            .then(result => args.helpers.sendOutput(args, result));
+        return args.helpers.sendOutput(args, iso);
     }
 });
