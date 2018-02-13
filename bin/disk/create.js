e306f376501b64a0a063d262814db76cd20277fd
Frederico Silva <frederico.silva@gmail.com>
2018-02-13 19:04:58 +0100

    disk create: add upload

--- bin/disk/create.js
+++ bin/disk/create.js
@@ -2,6 +2,12 @@
 
 const Cli = require('structured-cli');
 
+const websocketStream = require('lib/websocketStream');
+const vhdx = require('rbx-vhdx');
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
+
 const options = {
     name: {
         description: 'Disk name'
@@ -18,17 +24,53 @@ const options = {
       , type: 'int'
       , required: true
     }
+  , source: {
+        description: 'import disk path'
+      , type: 'string'
+      , required: false
+    }
 };
 
 module.exports = resource => Cli.createCommand('create', {
     description: 'Disk create'
   , plugins: resource.plugins
   , options: options
-  , handler: args => args.helpers.api
-        .post(resource.url(args), {
+  , handler: async args => {
+
+        const body = {
             name: args.name
           , service: args.type
           , size: args.size
-        })
-        .then(result => args.helpers.sendOutput(args, result))
+        };
+
+        if (args.source) {
+            body.metadata = {
+                source: {
+                    filename: path.basename(args.source)
+                  , size: fs.statSync(args.source).size
+                }
+            };
+        }
+
+        const disk = await args.helpers.api.post(resource.url(args), body);
+
+        if (args.source) {
+
+            const vhdxInfo = await util.promisify(vhdx.info)(args.source);
+
+            if (vhdxInfo.type !== 'dynamic') {
+                throw Cli.error.cancelled('<source> vhdx should be dynamic');
+            }
+
+            if (vhdxInfo.size > args.size * 1024**3) {
+                throw Cli.error.cancelled(`<source> ${Math.ceil(vhdxInfo.size/1024 **3)}GB is bigger than ${args.size}GB`);
+            }
+
+            const ws = await args.helpers.api.wsUpload(`/disk/${disk._id}/upload`);
+
+            await websocketStream.upload(ws, args.source);
+        }
+
+        return args.helpers.sendOutput(args, disk);
+    }
 });
