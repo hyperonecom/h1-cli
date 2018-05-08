50b9ddd944be5a72f6106fc04fdcc745b423fb4f
Frederico Silva <frederico.silva@gmail.com>
2018-05-08 13:28:44 +0200

    remove old code

--- bin/user/index.js
+++ bin/user/index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-/*
-const genericResource = require('bin/generic');
-
-const category = genericResource({
-    name: 'credentials'
-  , defaultQuery: '[].{id:_id,name:name,type:type,created:createdOn}'
-  , url: args => `${resource.url(args)}/certificate`
-  , commands: ['list', 'delete']
-  , params: resource.params
-});
-*/
-
 const Cli = require('structured-cli');
 
 const category = Cli.createCategory('user', {
