407766507efc9003590bea6949a90e59bbc146f7
Frederico Silva <frederico.silva@gmail.com>
2018-06-11 15:08:09 +0200

    read++

--- bin/vm/action/rename.js
+++ bin/vm/action/rename.js
@@ -5,28 +5,23 @@ const genericDefaults = require('bin/generic/defaults');
 
 const options = {
     newname: {
-        description: 'New name',
-        type: 'string',
-        required: true
+        description: 'New name'
+      , type: 'string'
+      , required: true
     }
 };
 
 module.exports = (resource) => Cli.createCommand('rename', {
-        description: `Rename ${resource.title}`,
-        plugins: genericDefaults.plugins,
-        options: Object.assign({}, resource.options, options),
-        params: resource.params,
-        handler: genericVMAction
-    });
-
-function genericVMAction(args) {
-
-    return args.helpers.api.post(`vm/${args.id}/actions`, {
-        name: 'rename',
-        data: {
-            newname: args.newname
-        }
-    })
-    .then(result => args.helpers.sendOutput(args, result));
-}
-
+    description: `Rename ${resource.title}`
+  , plugins: genericDefaults.plugins
+  , options: Object.assign({}, resource.options, options)
+  , params: resource.params
+  , handler: args => args.helpers.api
+        .post(`vm/${args.id}/actions`, {
+            name: 'rename'
+          , data: {
+                newname: args.newname
+            }
+        })
+        .then(result => args.helpers.sendOutput(args, result))
+});
