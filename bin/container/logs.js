1e85dcf293ab35556621730e29f65168a2ec6ac0
Frederico Silva <frederico.silva@gmail.com>
2018-06-12 18:46:05 +0200

    container logs: add follow

--- bin/container/logs.js
+++ bin/container/logs.js
@@ -2,12 +2,20 @@
 
 const Cli = require('structured-cli');
 
+const options = {
+    follow: {
+        description: 'Follow log output'
+      , type: 'boolean'
+      , required: false
+    }
+};
+
 module.exports = resource => Cli.createCommand('logs', {
     description: `${resource.title} logs`
   , plugins: resource.plugins
-  , options: resource.options
+  , options: Object.assign({}, resource.options, options)
   , handler: args => new Promise((resolve, reject) => args.helpers.api
-        .raw('get', `${resource.url(args)}/logs`)
+        .raw('get', `${resource.url(args)}/logs?follow=${args.follow}`)
         .pipe(process.stdout)
         .on('error', reject)
         .on('end', resolve)
