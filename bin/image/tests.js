d9f8a7b655ce95c564a1165ea19b16b10a9f4c36
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-07-04 22:28:13 +0200

    Fix image tests

--- bin/image/tests.js
+++ bin/image/tests.js
@@ -8,7 +8,7 @@ const now = Date.now();
 
 const name = `image-test-${now}`;
 
-const getCommon = async (test_name, options) => {
+const getCommon = async (test_name, options = {}) => {
     const image = options.image || 'debian';
     const name = `image-test-${test_name}-${now}`.replace(/[^\w]/g, '-');
     const password = await tests.getToken();
@@ -17,9 +17,9 @@ const getCommon = async (test_name, options) => {
         vm: await tests.run(`vm create --name ${name} --password ${password} --os-disk ${disk_name},ssd,10 --type a1.nano --image ${image}`),
         disk_name: disk_name,
         vm_name: name,
-        cleanup: () => {
-            tests.remove('vm', name);
-            tests.remove('disk', disk_name);
+        cleanup: async () => {
+            await tests.remove('vm', name);
+            await tests.remove('disk', disk_name);
         },
     };
 };
@@ -31,7 +31,7 @@ ava.test.serial('image rename', async t => {
         createParams: `--vm ${common.vm._id} --name ${name}`,
     })(t);
 
-    common.cleanup();
+    await common.cleanup();
 });
 
 ava.test.serial('image disk list', async t => {
@@ -41,7 +41,7 @@ ava.test.serial('image disk list', async t => {
     const list = await tests.run(`image disk --image ${image._id}`);
     t.true(list.some(d => d.type === 'ssd' && d.size === 10));
 
-    common.cleanup();
+    await common.cleanup();
     await tests.remove('image', image);
 
 });
@@ -51,7 +51,7 @@ ava.test.serial('image access', async t => {
 
     await tests.resourceAccessCycle('image', `--vm ${common.vm._id} --name ${name}`)(t);
 
-    common.cleanup();
+    await common.cleanup();
 });
 
 ava.test.serial('image resolver prefer server', async t => {
