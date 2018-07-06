3abcb27b5639e0c084c0c26c12eb90bb40f1768d
Frederico Silva <frederico.silva@gmail.com>
2018-07-06 14:49:50 +0200

    remove old tests

--- bin/test5.sh
+++
@@ -1,9 +0,0 @@
-set -euo pipefail
-set -x
-
-export H1_DEFAULT_OUTPUT='tsv'
-# export H1_DEFAULT_VERBOSE='true'
-
-h1 ip list --query "[].[_id]" | while read ip; do
-    h1 ip delete --yes $ip
-done
