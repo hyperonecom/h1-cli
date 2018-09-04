fc9f4d95179bef0538f839ffbf504b933e63e00f
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-09-05 00:59:21 +0200

    Update TravisCI & move CI secrets to web-UI

--- monitoring/deploy_image.sh
+++ monitoring/deploy_image.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
-if [ -z "$DOCKER_USERNAME" -o -z "$DOCKER_PASSWORD" -o -z "$DOCKER_REGISTRY" ]; then
+#!/bin/bash
+set -ueo pipefail
+
+if [ -z "${DOCKER_USERNAME}" -o -z "${DOCKER_PASSWORD}" -o -z "${DOCKER_REGISTRY}" ]; then
     echo "Missing environment variables. Required 'DOCKER_USERNAME', 'DOCKER_PASSWORD' and 'DOCKER_REGISTRY'.";
     exit 25;
 fi;
+
 docker-compose -f docker-compose.yml build web;
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY";
-docker image tag monitoring_web:latest "$DOCKER_REGISTRY/monitoring_web:latest";
-docker push "$DOCKER_REGISTRY/monitoring_web";
+TAG_NAME="${TRAVIS_BRANCH}";
+
+if [[ "${TRAVIS_EVENT_TYPE}" != "pull_request" && "${TRAVIS_BRANCH}" == "master" ]]; then
+    TAG_NAME="latest";
+fi;
+
+if [[ "${TRAVIS_EVENT_TYPE}" == "pull_request" && "$TRAVIS_BRANCH" == "latest" ]]; then
+    echo "Modification of the 'latest' tag by pull request is not allowed.";
+fi;
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin "${DOCKER_REGISTRY}";
+docker image tag monitoring_web:latest "${DOCKER_REGISTRY}/hyperone/monitoring_web:${TAG_NAME}";
+docker push "${DOCKER_REGISTRY}/hyperone/monitoring_web";
