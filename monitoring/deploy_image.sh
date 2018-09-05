7d2383bc85611c897b498dae3428b7f53d1b3491
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-09-05 02:39:00 +0200

    Update CI

--- monitoring/deploy_image.sh
+++ monitoring/deploy_image.sh
@@ -8,16 +8,6 @@ fi;
 
 docker-compose -f docker-compose.yml build web;
 
-TAG_NAME="${TRAVIS_BRANCH}";
-
-if [[ "${TRAVIS_EVENT_TYPE}" != "pull_request" && "${TRAVIS_BRANCH}" == "master" ]]; then
-    TAG_NAME="latest";
-fi;
-
-if [[ "${TRAVIS_EVENT_TYPE}" == "pull_request" && "$TRAVIS_BRANCH" == "latest" ]]; then
-    echo "Modification of the 'latest' tag by pull request is not allowed.";
-fi;
-
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin "${DOCKER_REGISTRY}";
 docker image tag monitoring_web:latest "${DOCKER_REGISTRY}/hyperone/monitoring_web:${TAG_NAME}";
 docker push "${DOCKER_REGISTRY}/hyperone/monitoring_web";
