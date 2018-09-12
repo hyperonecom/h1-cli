6427c4112c93b2db22cbfcdbcf32e73752b1b839
Adam Dobrawy <ad-m@users.noreply.github.com>
2018-09-12 17:49:47 +0200

    Add format 'id' in CLI (#176)
    * Add format 'ids' in CLI
    
    * Change format 'ids' to 'id'

--- scripts/cleanup_project.sh
+++ scripts/cleanup_project.sh
@@ -9,16 +9,16 @@ SKIPPED_CREDENTIALS=${H1_USER_CREDENTIALS}
     echo "Missing argument or environment variable H1_PROJECT"; exit -1;
 }
 
-h1 vm       list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 vm       delete --yes --vm
-h1 image    list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 image    delete --yes --image
-h1 disk     list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 disk     delete --yes --disk
-h1 netgw    list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 netgw    detach --netgw
-h1 netgw    list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 netgw    delete --yes --netgw
-h1 network  list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 network  delete --yes --network
-h1 ip       list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 ip       delete --yes --ip
-h1 iso      list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 iso      delete --yes --iso
-h1 firewall list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 firewall delete --yes --firewall
-h1 dns zone list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 dns zone delete --yes --zone
-h1 snapshot list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 snapshot delete --yes --snapshot
-h1 vault    list -o tsv --project-select $PROJECT --query '[].{id: _id}' | xargs -r -n 1 -P 8 h1 vault    delete --yes --vault
-h1 user credentials list -o tsv --query '[].{id: _id}' | grep -v "$SKIPPED_CREDENTIALS" | xargs -r -n 1 -P 8 h1 user credentials delete --yes --credentials
+h1 vm       list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vm       delete --yes --vm
+h1 image    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 image    delete --yes --image
+h1 disk     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 disk     delete --yes --disk
+h1 netgw    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw    detach --netgw
+h1 netgw    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw    delete --yes --netgw
+h1 network  list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 network  delete --yes --network
+h1 ip       list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 ip       delete --yes --ip
+h1 iso      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 iso      delete --yes --iso
+h1 firewall list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 firewall delete --yes --firewall
+h1 dns zone list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 dns zone delete --yes --zone
+h1 snapshot list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 snapshot delete --yes --snapshot
+h1 vault    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vault    delete --yes --vault
+h1 user credentials list -o id | grep -v "$SKIPPED_CREDENTIALS" | xargs -r -n 1 -P 8 h1 user credentials delete --yes --credentials
