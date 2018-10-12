14b097bd012a6f546f6cded323c759ef7afb4775
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-10-12 18:49:58 +0200

    Update replication ps1 error message

--- bin/replica/create/enable-replication.ps1
+++ bin/replica/create/enable-replication.ps1
@@ -33,91 +33,93 @@ function jobWait($job) {
     return $jobData
 }
 
-if ($VM -match '^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$') {
-    $VMobj = Get-VM -Id $VM
-} else {
-    $VMobj = Get-VM -Name $VM
+$VMobj = Get-VM -Id $VM -ErrorAction SilentlyContinue
+
+if($? -eq $false) {
+    $VMobj = Get-VM -Name $VM -ErrorAction SilentlyContinue
+    if($? -eq $false) {
+		Write-Error "Unable to identify VM"
+		return
+	}
 }
 
-if( $? ) {
+Write-Host "Disable Cert Revocation Check"
+New-ItemProperty `
+    -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization\Replication" `
+    -Name DisableCertRevocationCheck `
+    -Value 1 `
+    -Type DWORD `
+    -Force `
+    | Out-Null
 
-    Write-Host "Disable Cert Revocation Check"
-    New-ItemProperty `
-        -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization\Replication" `
-        -Name DisableCertRevocationCheck `
-        -Value 1 `
-        -Type DWORD `
-        -Force `
-        | Out-Null
+Write-Host "Importing CA"
+$certificate = Import-Certificate -FilePath $CACertificatePath -CertStoreLocation Cert:\LocalMachine\Root
 
-    Write-Host "Importing CA"
-    $certificate = Import-Certificate -FilePath $CACertificatePath -CertStoreLocation Cert:\LocalMachine\Root
+Write-Host "Importing Cert"
 
-    Write-Host "Importing Cert"
+$filePath = [System.IO.Path]::GetTempFileName() + '.pfx';
+[System.Convert]::FromBase64String($CertificateBase64) | Set-Content -Encoding Byte -Path $filePath
 
-    $filePath = [System.IO.Path]::GetTempFileName() + '.pfx';
-    [System.Convert]::FromBase64String($CertificateBase64) | Set-Content -Encoding Byte -Path $filePath
+$certificate = Import-PfxCertificate `
+    -FilePath $filePath `
+    -CertStoreLocation Cert:\LocalMachine\my `
+    -Password (ConvertTo-SecureString -String $Secret -Force -AsPlainText)
 
-    $certificate = Import-PfxCertificate `
-        -FilePath $filePath `
-        -CertStoreLocation Cert:\LocalMachine\my `
-        -Password (ConvertTo-SecureString -String $Secret -Force -AsPlainText)
+Remove-Item $filePath
 
-    Remove-Item $filePath
+<#
+Set-VMReplicationServer `
+    -ReplicationEnabled $true `
+    -AllowedAuthenticationType Certificate `
+    -ReplicationAllowedFromAnyServer $false `
+    -CertificateThumbprint $certificate.Thumbprint
+#>
 
-    <#
-    Set-VMReplicationServer `
-        -ReplicationEnabled $true `
-        -AllowedAuthenticationType Certificate `
-        -ReplicationAllowedFromAnyServer $false `
-        -CertificateThumbprint $certificate.Thumbprint
-    #>
+Write-Host "Testing Connection"
+Test-VMReplicationConnection `
+    -ReplicaServerName $ReplicaServerName `
+    -ReplicaServerPort 443 `
+    -AuthenticationType Certificate `
+    -CertificateThumbprint $certificate.Thumbprint
 
-    Write-Host "Testing Connection"
-    Test-VMReplicationConnection `
-        -ReplicaServerName $ReplicaServerName `
-        -ReplicaServerPort 443 `
-        -AuthenticationType Certificate `
-        -CertificateThumbprint $certificate.Thumbprint
+$VMID = $VMobj.Id
 
-    $VMID = $VMobj.Id
+$Msvm_ComputerSystem = Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_ComputerSystem -Filter "Name='$VMID'"
+$Msvm_ReplicationSettingData = $Msvm_ComputerSystem.GetRelated("Msvm_ReplicationSettingData") | % { $_ }
 
-    $Msvm_ComputerSystem = Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_ComputerSystem -Filter "Name='$VMID'"
-    $Msvm_ReplicationSettingData = $Msvm_ComputerSystem.GetRelated("Msvm_ReplicationSettingData") | % { $_ }
+$Msvm_ReplicationSettingData.RecoveryConnectionPoint = $ReplicaServerName
+$Msvm_ReplicationSettingData.AuthenticationType = 2 #1 - Kerberos, 2 - Certificate
+$Msvm_ReplicationSettingData.RecoveryServerPortNumber = 443
+$Msvm_ReplicationSettingData.CertificateThumbPrint = $certificate.Thumbprint
+$Msvm_ReplicationSettingData.CompressionEnabled = 1
+$Msvm_ReplicationSettingData.RecoveryHistory = 0
+$Msvm_ReplicationSettingData.ReplicationInterval = 300
 
-    $Msvm_ReplicationSettingData.RecoveryConnectionPoint = $ReplicaServerName
-    $Msvm_ReplicationSettingData.AuthenticationType = 2 #1 - Kerberos, 2 - Certificate
-    $Msvm_ReplicationSettingData.RecoveryServerPortNumber = 443
-    $Msvm_ReplicationSettingData.CertificateThumbPrint = $certificate.Thumbprint
-    $Msvm_ReplicationSettingData.CompressionEnabled = 1
-    $Msvm_ReplicationSettingData.RecoveryHistory = 0
-    $Msvm_ReplicationSettingData.ReplicationInterval = 300
+# 31 - SASDResourceTypeLogicalDisk
+$Msvm_VirtualSystemSettingData = $Msvm_ComputerSystem.GetRelated("Msvm_VirtualSystemSettingData")
+$Msvm_StorageAllocationSettingData = $Msvm_VirtualSystemSettingData.GetRelated("Msvm_StorageAllocationSettingData")
+$Msvm_ReplicationSettingData.IncludedDisks = $Msvm_StorageAllocationSettingData |
+    ? { $_.ResourceType -eq 31 } |
+    % { $_.__PATH }
 
-    # 31 - SASDResourceTypeLogicalDisk
-    $Msvm_VirtualSystemSettingData = $Msvm_ComputerSystem.GetRelated("Msvm_VirtualSystemSettingData")
-    $Msvm_StorageAllocationSettingData = $Msvm_VirtualSystemSettingData.GetRelated("Msvm_StorageAllocationSettingData")
-    $Msvm_ReplicationSettingData.IncludedDisks = $Msvm_StorageAllocationSettingData |
-        ? { $_.ResourceType -eq 31 } |
-        % { $_.__PATH }
+$Msvm_ReplicationService = Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_ReplicationService
 
-    $Msvm_ReplicationService = Get-WmiObject -Namespace root\virtualization\v2 -Class Msvm_ReplicationService
+$ret = $Msvm_ReplicationService.CreateReplicationRelationship($Msvm_ComputerSystem, $Msvm_ReplicationSettingData.GetText(2))
 
-    $ret = $Msvm_ReplicationService.CreateReplicationRelationship($Msvm_ComputerSystem, $Msvm_ReplicationSettingData.GetText(2))
+if ($ret.ReturnValue -eq 4096) {
 
-    if ($ret.ReturnValue -eq 4096) {
+    $JobData = jobWait($ret.job)
 
-        $JobData = jobWait($ret.job)
-
-        if ($JobData.Status -ne "OK") {
-            Write-Error $JobData
-            return
-        }
+    if ($JobData.Status -ne "OK") {
+        # Write-Error $JobData
+        Write-Error $JobData.ErrorDescription
+        return
     }
+}
 
-    Write-Host "Replication successfully enabled"
+Write-Host "Replication successfully enabled"
 
-    if ($Autostart -eq $true){
-        Start-VMInitialReplication -VMName $VM
-        Write-Host "Replication successfully started"
-    }
+if ($Autostart -eq $true){
+    Start-VMInitialReplication -VMName $VM
+    Write-Host "Replication successfully started"
 }
