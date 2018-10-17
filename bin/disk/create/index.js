dfd60b1444aedd67568f8948355e69ce847d57c4
Frederico Silva <frederico.silva@gmail.com>
2018-10-17 16:00:56 +0200

    fix: disk create from source-file

--- bin/disk/create/index.js
+++ bin/disk/create/index.js
@@ -54,7 +54,7 @@ module.exports = resource => Cli.createCommand('create', {
         }
 
         if (args['source-file']) {
-            const vhdxInfo = await vhdx.info(['source-file']);
+            const vhdxInfo = await vhdx.info(args['source-file']);
 
             if (body.size === null) {
                 body.size = Math.ceil(vhdxInfo.size / 1024**3); // B -> GiB
