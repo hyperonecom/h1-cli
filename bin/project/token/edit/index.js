b64532d64d7acc640908032c6dc272ff56e07fb7
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-10-19 12:11:20 +0200

    Extract prompt_yaml_editor

--- bin/project/token/edit/index.js
+++ bin/project/token/edit/index.js
@@ -5,6 +5,25 @@ const inquirer = require('inquirer');
 const yaml = require('js-yaml');
 
 
+async function prompt_yaml_editor(options) {
+    const data = await inquirer.prompt({
+        type: 'editor',
+        name: 'input',
+        message: options.message,
+        default: yaml.safeDump(options.defaultValue),
+        validate: function (text) {
+            let content;
+            try {
+                content = yaml.safeLoad(text);
+            } catch (e) {
+                return `You can not change the data format (YAML). The data entered is not a valid YAML document.\n${e.message}`;
+            }
+            return options.validator ? options.validator(content) : true;
+        },
+    });
+    return yaml.safeLoad(data.input);
+}
+
 module.exports = resource => {
     const options = {
         [resource.name]: {
@@ -22,44 +41,30 @@ module.exports = resource => {
         handler: async args => {
             const url = `${resource.url(args)}/${args[resource.name]}`;
             const result = await args.helpers.api.get(url);
-
-            const questions = {
-                type: 'editor',
-                name: 'input',
-                message: 'Specify the name and access policy of the token.',
-                default: yaml.safeDump({
+            const input = await prompt_yaml_editor({
+                defaultValue: {
                     name: result.name,
                     access: result.access.map(access => ({
                         method: access.method,
                         path: access.path,
                     })),
-                }),
-                validate: function (text) {
-                    try {
-                        yaml.safeLoad(text);
-                    } catch (e) {
-                        return `You can not change the data format (YAML). The data entered is not a valid YAML document.\n${e.message}`;
-                    }
-                    return true;
                 },
-            };
-
-            const {input} = await inquirer.prompt(questions);
-            const data = yaml.safeLoad(input);
+                message: 'Specify the name and access policy of the token.',
+            });
             const requests = [];
-            if ('name' in data && data.name !== result.name) {
+            if ('name' in input && input.name !== result.name) {
                 requests.push(args.helpers.api.patch(`${args.$node.parent.config.url(args)}/${args[resource.name]}`, {
-                    name: data.name,
+                    name: input.name,
                 }));
             }
-            if ('access' in data) {
+            if ('access' in input) {
                 // Add new access
-                requests.push(...data.access
+                requests.push(...input.access
                     .filter(access => !result.access.find(acc => acc.path === access.path && acc.method === access.method))
                     .map(access => args.helpers.api.post(`${url}/access`, access)));
                 // Remove missing access
                 requests.push(...result.access
-                    .filter(access => !data.access.find(acc => acc.path === access.path && acc.method === access.method))
+                    .filter(access => !input.access.find(acc => acc.path === access.path && acc.method === access.method))
                     .map(access => args.helpers.api.delete(`${url}/access/${access._id}`)));
             }
             for (const req in requests) {
