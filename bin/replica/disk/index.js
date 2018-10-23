97a47cb7deaa3a035565548c7f292ad01e72d5e6
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-10-23 14:14:39 +0200

    Add command 'replica disk'

---
+++ bin/replica/disk/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const genericResource = require('bin/generic');
+const text = require('lib/text');
+
+module.exports = resource => {
+    const options = {
+        [resource.name]: {
+            description: `${text.toTitleCase(resource.title)} name or ID`,
+            type: 'string',
+            required: true,
+        },
+    };
+
+    const subresource = {
+        title: `disk of ${resource.title}`,
+        name: 'disk',
+        defaultQuery: '[].{id:disk._id, location: _id, size:disk.size, type:disk.type}',
+        url: args => `${resource.url(args)}/${args.replica}/hdd`,
+        commands: ['list'],
+        options: options,
+    };
+
+    return genericResource(subresource);
+};
