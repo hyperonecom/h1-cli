e5db22c8ca538909d8a899fae131dd31fb3610c4
Adam Dobrawy <ad-m@users.noreply.github.com>
2018-11-14 20:44:46 +0100

    test: message flow in webSocket (#269)

--- bin/config/tests.js
+++ bin/config/tests.js
@@ -3,6 +3,7 @@ const ava = require('ava');
 
 require('../../scope/h1');
 const tests = require('../../lib/tests');
+const api = require('../../lib/api');
 
 const now = Date.now();
 
@@ -20,4 +21,33 @@ ava('config set & get & show & unset', async t => {
     await tests.run(`config unset --key ${key}`);
     const missing = await tests.run(`config get --key ${key}`);
     t.true(missing === 'key not set');
+});
+
+ava('test websocket message', async t => {
+    // TODO: Move to proper monitoring system
+    const resource = await tests.run('ip create');
+    const ws = await api.getWS();
+    t.true(!!ws);
+    const promise = new Promise((resolve, reject) => {
+        ws.on('message', msg => {
+
+            msg = JSON.parse(msg);
+
+            if (msg.message.type !== 'event') {
+                return;
+            }
+
+            const event = msg.message.data;
+
+            if (resource._id === msg.resource.data._id) {
+                if (event.state === 'finished') {
+                    return resolve(resource);
+                } else if (event.state === 'error') {
+                    return reject(resource);
+                }
+            }
+        });
+    }).finally(() => ws.close());
+    await tests.remove('ip', resource);
+    await promise;
 });
