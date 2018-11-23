4bced4d3c7df3e817b94bf36ef37a94258aca5e1
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-11-23 14:25:50 +0100

    Update flavour of reservation & vm in reservation

--- bin/reservation/tests.js
+++ bin/reservation/tests.js
@@ -6,38 +6,42 @@ const tests = require('../../lib/tests');
 
 const now = Date.now();
 
+const reservation_flavour = '_dev.pico, 1 day';
+const vm_flavour = '_dev.pico';
+
 ava.serial('reservation life cycle', tests.resourceLifeCycle('reservation', {
-    createParams: `--name reservation-life-cycle-${now} --type "m2.tiny, 1 year"`,
+    createParams: `--name reservation-life-cycle-${now} --type "${reservation_flavour}"`,
     stateCreated: 'Detached',
     skipDelete: true,
 }));
 
 ava.serial('reservation assign limits', async t => {
-    const reservation = await tests.run(`reservation create --name reservation-${now} --type 'm2.tiny, 1 year'`);
-    t.true(reservation.state === 'Detached');
+    const reservation = await tests.run(`reservation create --name reservation-${now} --type '${reservation_flavour}'`);
 
     const reversation_list = await tests.run('reservation list');
     t.true(reversation_list.some(x => x._id === reservation._id));
     const token = await tests.getToken();
 
-    const common_vm_params = `--os-disk ssd,10 --type m2.tiny --image debian --password ${token}`;
+    const common_vm_params = `--type ${vm_flavour} --no-start --password ${token}`;
     const vm_wrong_type = await tests.run(`vm create --name reservation-vm-wrong-type-${now} ${common_vm_params}`);
     const vm_valid = await tests.run(`vm create --name reservation-vm-${now} ${common_vm_params} `);
     const vm_over_limit = await tests.run(`vm create --name reservation-vm-over-limit-${now} ${common_vm_params}`);
 
-    await t.throwsAsync(() => tests.run(`reservation assign --reservation ${reservation.name} --resource ${vm_valid._id}`));
-    await tests.run(`reservation assign --reservation ${reservation.name} --resource ${vm_over_limit._id}`);
+    await tests.run(`reservation assign --reservation ${reservation.name} --resource ${vm_valid._id}`);
+    await t.throwsAsync(() => tests.run(`reservation assign --reservation ${reservation.name} --resource ${vm_over_limit._id}`));
     await t.throwsAsync(() => tests.run(`reservation assign --reservation ${reservation.name} --resource ${vm_wrong_type._id}`));
 
     const assigned = await tests.run(`reservation show --reservation ${reservation._id}`);
     t.true(assigned.assigned ===vm_valid._id);
+    t.true(assigned.state === 'Detached');
+
     await tests.remove('vm', vm_valid);
     await tests.remove('vm', vm_over_limit);
     await tests.remove('vm', vm_wrong_type);
 });
 
 ava.serial('reservation extend', async t => {
-    const reservation = await tests.run(`reservation create --name reservation-${now} --type 'm2.tiny, 1 year'`);
+    const reservation = await tests.run(`reservation create --name reservation-${now} --type '${reservation_flavour}'`);
     t.true(reservation.state === 'Detached');
     await tests.run(`reservation extend --reservation ${reservation.name}`);
     await t.throwsAsync(() => tests.run(`reservation extend --reservation ${reservation.name}`));
