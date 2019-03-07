9a69a4503a6c952fc636737a8bb8acb490b95504
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-03-07 08:23:21 +0100

    Read container logs to stdout

--- bin/container/log/index.js
+++ bin/container/log/index.js
@@ -6,6 +6,6 @@ module.exports = resource => Cli.createCommand('log', {
     dirname: __dirname,
     plugins: resource.plugins,
     options: resource.options,
-    handler: args => args.helpers.api.get(`${resource.url(args)}/log`)
-        .then(resource => args.helpers.sendOutput(args, resource)),
+    handler: args => args.helpers.api.api('GET', `${resource.url(args)}/log`).buffer(false)
+        .then(resp => resp.pipe(process.stdout)).then(() => {}),
 });
