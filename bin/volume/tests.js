ce0bd138c0f53617d20ed023189053d6718692ec
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-03-08 14:33:54 +0100

    Add tests for volume

---
+++ bin/volume/tests.js
@@ -0,0 +1,15 @@
+'use strict';
+const ava = require('ava');
+
+require('../../scope/h1');
+const tests = require('../../lib/tests');
+
+const now = Date.now();
+
+const name = `volume-test-${now}`;
+const createParams = `--name ${name} --type volume --size 10`;
+
+ava.serial('volume life cycle', tests.resourceLifeCycle('volume', {
+    createParams: createParams,
+    stateCreated: 'Detached',
+}));
