1da0cb754eb116837085377e87c2d234148dc45b
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-03-11 19:10:28 +0100

    Update container process list

--- bin/container/process/index.js
+++ bin/container/process/index.js
@@ -3,20 +3,16 @@
 const genericDefaults = require('bin/generic/defaults');
 const genericResource = require('bin/generic');
 
-module.exports = (parent) => {
-    const resource = {
-        name: 'process',
-        defaultQuery: '[].{name:name, user:user, process_id:process_id, parent_process_id:parent_process_id}',
-        url: args => `${parent.url(args)}/process`,
-        plugins: genericDefaults.plugins,
-        commands: ['list'],
-        options: parent.options,
-        earlyAdoptersOnly: true,
-        title: 'process in container',
-        context: {
-            listParams: `--${parent.name} my-${parent.name}`,
-        },
-    };
-
-    return genericResource(resource);
-};
+module.exports = (parent) => genericResource({
+    name: 'process',
+    defaultQuery: '[].{id:id, parent:parent, user:user, name:name}',
+    url: args => `${parent.url(args)}/process`,
+    plugins: genericDefaults.plugins,
+    commands: ['list'],
+    options: parent.options,
+    earlyAdoptersOnly: true,
+    title: 'process in container',
+    context: {
+        listParams: `--${parent.name} my-${parent.name}`,
+    },
+});
