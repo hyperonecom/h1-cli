d74a2cb24661716d7f358aef7876918a089111f6
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-04-06 21:41:13 +0200

    Add IP create with initial ptrRecord

--- bin/ip/create/index.js
+++ bin/ip/create/index.js
@@ -3,16 +3,31 @@
 const Cli = require('lib/cli');
 const genericDefaults = require('bin/generic/defaults');
 
-const handler = (args) => args.helpers.api
-    .post('ip', {
-        tag: require('lib/tags').createTagObject(args.tag),
-    })
-    .then(result => args.helpers.sendOutput(args, result));
+const options = {
+    'ptr-record': {
+        description: 'Value of PTR record',
+        type: 'string',
+        required: true,
+    },
+};
 
 module.exports = resource => Cli.createCommand('create', {
     dirname: __dirname,
     genericOptions: ['tag'],
     description: `Create ${resource.title}`,
     plugins: genericDefaults.plugins,
-    handler: handler,
+    options: Object.assign({}, options, resource.options),
+    handler: (args) => {
+        const body = {
+            tag: require('lib/tags').createTagObject(args.tag),
+        };
+
+        if (args['ptr-record']) {
+            body.ptrRecord = args['ptr-record'];
+        }
+
+        return args.helpers.api
+            .post('ip', body)
+            .then(result => args.helpers.sendOutput(args, result));
+    },
 });
