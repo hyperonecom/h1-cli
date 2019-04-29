e33fd217bad275112c53c568e97b206b39d9c0b5
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-04-29 17:20:38 +0200

    Add 'website sftp'

--- bin/website/index.js
+++ bin/website/index.js
@@ -30,6 +30,7 @@ const credential_type = ['certificate', 'password'];
 const category = genericResource(resource);
 
 category.addChild(require('./create')(resource));
+category.addChild(require('./sftp')(resource));
 
 category.addChild(require('../generic/credential')(resource, credential_type));
 
---
+++ bin/website/sftp/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Cli = require('lib/cli');
+
+const options = {
+    website: {
+        description: 'Website ID or name',
+        type: 'string',
+        required: true,
+    },
+};
+
+module.exports = resource => Cli.createCommand('sftp', {
+    description: `Connect to ${resource.title} using SFTP`,
+    plugins: resource.plugins,
+    dirname: __dirname,
+    options: Object.assign({}, resource.options, options),
+    handler: args => args.helpers.api
+        .get(`${resource.url(args)}/${args.website}`)
+        .then(result => {
+            const sshArgs = [
+                `${result._id}@${result._id}.website.${result.project}.pl-waw-1.hyperone.cloud`,
+            ];
+
+            console.log(`sftp ${sshArgs.join(' ')}`);
+
+            const spawn = require('child_process').spawn;
+
+            return new Promise((resolve, reject) => {
+                const ssh = spawn('sftp', sshArgs, { stdio: 'inherit' });
+
+                ssh.on('close', resolve);
+                ssh.on('error', reject);
+            });
+        }),
+});
