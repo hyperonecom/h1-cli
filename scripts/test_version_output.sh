08cddf84249d90ef0407335f6dbde83166dadbd8
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-05-07 15:42:13 +0200

    Add tests for extra outputs (#322)
    * Add tests for extra outputs
    
    * Set exit code in test_version_output.sh

---
+++ scripts/test_version_output.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+BINARY=$1
+expected_version=$(jq '.version' package.json -r)
+output=$($1 --version 2>&1);
+
+if [ "$output" != "$expected_version" ]; then
+	echo "The binary '$BINARY' did not return the correct output of version.";
+	echo "Returned output":
+	echo $output;
+	exit 2
+fi
