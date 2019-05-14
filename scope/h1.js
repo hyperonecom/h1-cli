0108434496343914b98d1d7388cfc37383552b60
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-05-14 14:19:18 +0200

    Fix scope full name

--- scope/h1.js
+++ scope/h1.js
@@ -1,5 +1,5 @@
 'use strict';
 
 process.env.SCOPE_NAME = 'h1';
-process.env.SCOPE_FULL_NAME = 'HYPERONE';
+process.env.SCOPE_FULL_NAME = 'HyperOne';
 process.env.API_HOSTNAME = process.env.API_HOSTNAME || 'api.hyperone.com';
