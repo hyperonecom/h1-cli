07e4549a831e7c6e2c3bf2a9e2a073ec4907bc1b
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-06-05 12:34:44 +0200

    Fix revoke orrganisation access in test

--- scripts/revoke_user.sh
+++ scripts/revoke_user.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-set -eux
+set -ux
+# Missing "-e" to attempt revoke organisation access even
+# when project access revoke fail
 USERNAME="$1"
 PROJECT="$2";
 ORGANISATION=$(h1 project show --project "$PROJECT" --query '[].{o:organisation}' -o tsv 2>/dev/null)
 
-h1 project access revoke --email "$USERNAME" --project "$PROJECT";
-h1 organisation access revoke --email "$USERNAME" --organisation "$ORGANISATION"
+h1 project access revoke -v --email "$USERNAME" --project "$PROJECT";
+h1 organisation access revoke -v --email "$USERNAME" --organisation "$ORGANISATION"
