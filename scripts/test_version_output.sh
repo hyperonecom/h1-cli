3dbe516e30c06eb048de213701275487644a3a80
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-06-11 15:44:10 +0200

    Add tests for Alpine binaries output

--- scripts/test_version_output.sh
+++ scripts/test_version_output.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-set -eu
+set -eux
 
-BINARY=$1
+BINARY=$(realpath $1)
+MODE=${2:-native}
 expected_version=$(jq '.version' package.json -r)
-output=$($1 --version 2>&1);
+
+if [ "$MODE" == "alpine" ]; then
+	output=$(docker run -v $BINARY:/bin/h1 node:alpine /bin/h1 --version);
+else
+	output=$($BINARY --version 2>&1);
+fi;
 
 if [ "$output" != "$expected_version" ]; then
 	echo "The binary '$BINARY' did not return the correct output of version.";
