74003db04774ad103d1f765b00a95e75ec4928af
yrself <takise@protonmail.com>
2019-06-21 23:38:32 +0200

    Changed url in childDefaults for repository index.js

--- bin/registry/repository/index.js
+++ bin/registry/repository/index.js
@@ -30,7 +30,7 @@ module.exports = (parent) => {
                 },
             }
         ),
-        url: args => `${resource.url(args)}/${args[resource.name]}`,
+        url: args => `/${args[resource.name]}`,
         dirname: __dirname,
     });
 
