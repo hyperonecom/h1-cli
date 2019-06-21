cc18fd8e1ffd6ed1c4e43d681d0694c63a78b69b
yrself <takise@protonmail.com>
2019-06-21 23:09:56 +0200

    Modified /registry/repository/index.js

--- bin/registry/repository/index.js
+++ bin/registry/repository/index.js
@@ -10,11 +10,29 @@ module.exports = (parent) => {
         url: args => `${parent.url(args)}/repository`,
         options: parent.options,
         plugins: genericDefaults.plugins,
-        commands: ['list', 'tag'],
+        commands: ['list'],
         extraCommands: [],
         title: 'Repository of Registry',
     };
     const category = genericResource(resource);
+
+    const childDefaults = Object.assign({}, resource, {
+        options: Object.assign(
+            {},
+            parent.options,
+            {
+                [resource.name]: {
+                    description: `${text.toTitleCase(resource.title)} ID or name`,
+                    type: 'string',
+                    required: true,
+                },
+            }
+        ),
+        url: args => `${resource.url(args)}/${args[resource.name]}`,
+        dirname: __dirname,
+    });
+
+    category.addChild(genericAction(childDefaults, 'tag'));
 
     return category;
 };
