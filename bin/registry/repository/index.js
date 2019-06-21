422164cef915220bea8a82040643114895c9423b
yrself <takise@protonmail.com>
2019-06-21 23:59:05 +0200

    Revert "Changed url in childDefaults for repository index.js"
    This reverts commit 2e5a36d7de076491f837133dbbd64e30d263507a.

--- bin/registry/repository/index.js
+++ bin/registry/repository/index.js
@@ -30,7 +30,7 @@ module.exports = (parent) => {
                 },
             }
         ),
-        url: args => `/${args[resource.name]}`,
+        url: args => `${resource.url(args)}/${args[resource.name]}`,
         dirname: __dirname,
     });
 
