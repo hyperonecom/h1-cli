370153386a15b6b6dabf1291bfc85d3645d4a7b0
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-06-28 18:41:52 +0200

    Update registry path

--- monitoring/deploy_image.sh
+++ monitoring/deploy_image.sh
@@ -6,8 +6,5 @@ if [ -z "${DOCKER_USERNAME}" -o -z "${DOCKER_PASSWORD}" -o -z "${DOCKER_REGISTRY
     exit 25;
 fi;
 
-docker-compose -f docker-compose.yml build web;
-
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin "${DOCKER_REGISTRY}";
-docker image tag monitoring_web:latest "${DOCKER_REGISTRY}/hyperone/monitoring_web:${TAG_NAME}";
-docker push "${DOCKER_REGISTRY}/hyperone/monitoring_web";
+docker-compose build web;
+docker-compose push web;
