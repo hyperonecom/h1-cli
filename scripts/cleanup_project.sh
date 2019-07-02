4eb7111325acd6b9f37c310e0e448abbcebc17c4
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-07-02 12:09:29 +0200

    tests: Cleanup network (#363)
    * Drop domain on create in website
    
    * Add 'network firewall remove' to cleanup_project

--- scripts/cleanup_project.sh
+++ scripts/cleanup_project.sh
@@ -8,11 +8,13 @@ SKIPPED_CREDENTIALS=${HYPERONE_USER_CREDENTIALS:--}
 [ -z "$PROJECT" ] && {
     echo "Missing argument or environment variable HYPERONE_PROJECT"; exit 64;
 }
-
+# Decompose resources
+h1 network      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 network firewall remove --network
+h1 netgw        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw       detach --netgw
+# Delete all resources
 h1 vm           list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vm          delete --yes --vm
 h1 image        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 image       delete --yes --image
 h1 disk         list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 disk        delete --yes --disk
-h1 netgw        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw       detach --netgw
 h1 netgw        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw       delete --yes --netgw
 h1 network      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 network     delete --yes --network
 h1 ip           list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 ip          delete --yes --ip
@@ -27,7 +29,7 @@ h1 volume       list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vol
 h1 container    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 container   delete --yes --container
 h1 agent        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 agent       delete --yes --agent
 h1 website      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 website     delete --yes --website
-h1 database     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 database     delete --yes --database
+h1 database     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 database    delete --yes --database
 h1 user credentials list -o id | grep -v "$SKIPPED_CREDENTIALS" | xargs -r -n 1 h1 user credentials delete --yes --credentials
 h1 project credentials list --project "$PROJECT" -o id | grep -v "$SKIPPED_CREDENTIALS" | xargs -r -n 1 h1 project credentials delete --yes --project "$PROJECT" --credentials
 h1 project notification credits list --project "$PROJECT" -o id | xargs -r -n 1 h1 project notification credits delete --project "$PROJECT" --limit
