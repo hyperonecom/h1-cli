fb53341160c6aea1aa4cc2762aed9bec432fb231
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-07-11 15:50:35 +0200

    Fix --project-select update selected project

--- bin/_plugins/loginRequired/index.js
+++ bin/_plugins/loginRequired/index.js
@@ -2,28 +2,28 @@
 
 const config = require('lib/config');
 const Cli = require('lib/cli');
+const api = require('lib/api');
 const logger = require('lib/logger');
 
 const login = require('bin/login');
 
-const api = require('../api');
+const apiPlugin = require('../api');
 
 module.exports = {
     onBeforeHandler: context => {
         const profile = config.get('profile', {});
 
-        if (config.get_token()) {
+        if (config.getTokenEnv()) {
+            api.setToken(config.getTokenEnv());
             console.error(`Skip log in. Using ${config.scope()}_ACCESS_TOKEN_SECRET from environment variable`);
             return;
-        }
-        if (profile.apiKey && profile.expires && new Date(profile.expires) > Date.now()) {
+        } else if (profile.apiKey && profile.expires && new Date(profile.expires) > Date.now()) {
+            api.setToken(profile.apiKey);
             context.args.profile = profile;
-            context.args.apiKey = profile.apiKey;
             return;
         }
-
         // Hack to reuse login handler
-        api.onBeforeHandler(context);
+        apiPlugin.onBeforeHandler(context);
 
         const username = context.args.username;
 
@@ -36,7 +36,6 @@ module.exports = {
 
         return login
             .handler(context.args)
-            .then(() => context.args.username = username)
-        ;
+            .then(() => context.args.username = username);
     },
 };
