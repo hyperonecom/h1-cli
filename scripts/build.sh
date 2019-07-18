5993e830cb7c782dd6312952c320a1dcc3ff6b1e
Hyperone <hyperone@Hyperones-MacBook-Air.local>
2019-07-18 18:26:30 +0200

    Initial commit
    generic.log: Add tests for stream of log
    
    generic.update,website.domain: Add tests for management of domain
    
    generic.tag: mark --tag in 'tag add' as required
    
    registry: add tests to verify our container compatibility
    
    generic.*.add: show only the new added tag
    
    Change in build.sh
    
    Change in build.sh
    
    Change in build.sh
    
    Change in .travis.yml
    
    Change in .travis.yml
    
    Change in .travis.yml
    
    Change in .travis.yml
    
    Change in .travis.yml

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,21 @@ DOCKER_GID=$(id -g);
 NODE_VERSION="12"
 
 for scope in h1 rbx; do
-	npx pkg -c ./package.json -t "node${NODE_VERSION}-linux" -o "./dist/${scope}-linux" "./bin/${scope}";
-	npx pkg -c ./package.json -t "node${NODE_VERSION}-macos" -o "./dist/${scope}-macos" "./bin/${scope}";
-	npx pkg -c ./package.json -t "node${NODE_VERSION}-win" -o "./dist/${scope}-win.exe" "./bin/${scope}";
-	docker run -v $(pwd):/src -w /src "node:${NODE_VERSION}-alpine" npx pkg -c ./package.json -t "node${NODE_VERSION}-alpine" -o "./dist/${scope}-alpine" "./bin/${scope}";
+	npx pkg -c ./package.json -t "node${NODE_VERSION}-linux" -o "./dist/${scope}" "./bin/${scope}";
+	tar czf ./dist/${scope}-linux.tar.gz -C ./dist/ ${scope}
+	rm ./dist/${scope}
+	
+	npx pkg -c ./package.json -t "node${NODE_VERSION}-macos" -o "./dist/${scope}" "./bin/${scope}";
+	tar czf ./dist/${scope}-macos.tar.gz -C ./dist/ ${scope}
+	rm ./dist/${scope}
+
+	npx pkg -c ./package.json -t "node${NODE_VERSION}-win" -o "./dist/${scope}.exe" "./bin/${scope}";
+	zip -j ./dist/${scope}-win.zip ./dist/${scope}.exe
+	rm ./dist/${scope}.exe
+
+	docker run -v $(pwd):/src -w /src "node:${NODE_VERSION}-alpine" npx pkg -c ./package.json -t "node${NODE_VERSION}-alpine" -o "./dist/${scope}" "./bin/${scope}";
+	tar czf ./dist/${scope}-alpine.tar.gz -C ./dist/ ${scope}
+	rm -f ./dist/${scope}
 done;
 sudo chown "$DOCKER_UID:$DOCKER_GID" ./dist/*;
 chmod +x ./dist/*;
