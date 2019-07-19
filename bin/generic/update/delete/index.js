3bf0771c924d82798e8492f6ddb6a22338b07e59
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-07-20 00:54:16 +0200

    generic.update,website.domain: Add tests for management of domain

--- bin/generic/update/delete/index.js
+++ bin/generic/update/delete/index.js
@@ -25,11 +25,11 @@ module.exports = (resource, field_name) => {
             const obj = await args.helpers.api
                 .get(`${resource.url(args)}/${args[resource.name]}`);
             const value = obj[field_name] || [];
-            return args.helpers.api
+            const result = await args.helpers.api
                 .post(`${resource.url(args)}/${args[resource.name]}/actions/update_${field_name}`, {
-                    [field_name]: value.filter(x => args[name].includes(x)),
-                })
-                .then(result => args.helpers.sendOutput(args, result));
+                    [field_name]: value.filter(x => !args[name].includes(x)),
+                });
+            return args.helpers.sendOutput(args, result[field_name]);
         },
     });
 };
