f45a68475184099a758eb650079820c623d85015
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-08-08 12:32:05 +0200

    Update travis_deploy.sh

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin "${DOCKER_REGISTRY}"
   docker build . -t "${DOCKER_REGISTRY}/cli:${TAG_NAME}"
   docker push "${DOCKER_REGISTRY}/cli:${TAG_NAME}"
-  docker run --rm "${DOCKER_REGISTRY}/cli:${TAG_NAME}" -- --help
+  docker run --rm "${DOCKER_REGISTRY}/cli:${TAG_NAME}" h1 --help
   if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
     cd ./monitoring;
     ./deploy_image.sh;
