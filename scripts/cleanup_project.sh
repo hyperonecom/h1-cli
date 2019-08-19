c50db2558da6ae13f0434e078767748b91ad8ec4
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-19 13:47:02 +0200

    cleanup: Fix cleanup of slave project

--- scripts/cleanup_project.sh
+++ scripts/cleanup_project.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script designed to remove user's resources and data left after testing to ensure correct
 # future testing in a clean environment.
-
+set -x
 PROJECT=${HYPERONE_PROJECT:-$1}
 SKIPPED_CREDENTIALS=${HYPERONE_USER_CREDENTIALS:--}
 
@@ -16,25 +16,25 @@ h1 website list --project-select $PROJECT -o id | while read WEBSITE; do
 	xargs -r -n 1 -P 8 h1 website snapshot delete --yes --project-select $PROJECT --website "$WEBSITE" --snapshot;
 done;
 # Delete all resources
-h1 vm           list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vm          delete --yes --vm
-h1 image        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 image       delete --yes --image
-h1 disk         list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 disk        delete --yes --disk
-h1 netgw        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw       delete --yes --netgw
-h1 network      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 network     delete --yes --network
-h1 ip           list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 ip          delete --yes --ip
-h1 iso          list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 iso         delete --yes --iso
-h1 firewall     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 firewall    delete --yes --firewall
-h1 dns zone     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 dns zone    delete --yes --zone
-h1 snapshot     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 snapshot    delete --yes --snapshot
-h1 vault        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vault       delete --yes --vault
-h1 log          list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 log         delete --yes --log
-h1 reservation  list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 reservation delete --yes --reservation
-h1 volume       list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 volume      delete --yes --volume
-h1 container    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 container   delete --yes --container
-h1 agent        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 agent       delete --yes --agent
-h1 website      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 website     delete --yes --website
-h1 database     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 database    delete --yes --database
-h1 registry     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 registry    delete --yes --registry
+h1 vm           list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vm          delete --project-select $PROJECT --yes --vm
+h1 image        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 image       delete --project-select $PROJECT --yes --image
+h1 disk         list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 disk        delete --project-select $PROJECT --yes --disk
+h1 netgw        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 netgw       delete --project-select $PROJECT --yes --netgw
+h1 network      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 network     delete --project-select $PROJECT --yes --network
+h1 ip           list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 ip          delete --project-select $PROJECT --yes --ip
+h1 iso          list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 iso         delete --project-select $PROJECT --yes --iso
+h1 firewall     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 firewall    delete --project-select $PROJECT --yes --firewall
+h1 dns zone     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 dns zone    delete --project-select $PROJECT --yes --zone
+h1 snapshot     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 snapshot    delete --project-select $PROJECT --yes --snapshot
+h1 vault        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 vault       delete --project-select $PROJECT --yes --vault
+h1 log          list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 log         delete --project-select $PROJECT --yes --log
+h1 reservation  list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 reservation delete --project-select $PROJECT --yes --reservation
+h1 volume       list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 volume      delete --project-select $PROJECT --yes --volume
+h1 container    list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 container   delete --project-select $PROJECT --yes --container
+h1 agent        list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 agent       delete --project-select $PROJECT --yes --agent
+h1 website      list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 website     delete --project-select $PROJECT --yes --website
+h1 database     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 database    delete --project-select $PROJECT --yes --database
+h1 registry     list --project-select $PROJECT -o id | xargs -r -n 1 -P 8 h1 registry    delete --project-select $PROJECT --yes --registry
 h1 user credentials list -o id | grep -v "$SKIPPED_CREDENTIALS" | xargs -r -n 1 h1 user credentials delete --yes --credentials
 h1 project credentials list --project "$PROJECT" -o id | grep -v "$SKIPPED_CREDENTIALS" | xargs -r -n 1 h1 project credentials delete --yes --project "$PROJECT" --credentials
 h1 project notification credits list --project "$PROJECT" -o id | xargs -r -n 1 h1 project notification credits delete --project "$PROJECT" --limit
