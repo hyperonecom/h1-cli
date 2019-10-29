c3f04ec06737c52efeb5c129a44d4c7676bcd7d7
Frederico Silva <frederico.silva@gmail.com>
2019-10-29 16:27:15 +0100

    deps: update @hyperone/eslint-config

--- bin/vm/console/index.js
+++ bin/vm/console/index.js
@@ -3,5 +3,5 @@
 const genericConsole = require('bin/generic/console');
 
 module.exports = resource => genericConsole(resource,
-    (args) => args.helpers.api.redirect_url(`${resource.url(args)}/console`),
+    (args) => args.helpers.api.redirect_url(`${resource.url(args)}/console`)
 );
