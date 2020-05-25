4b0b85f03fd887071fd64ac37369e905a53327d2
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-05-25 14:30:00 +0200

    vm@tests: Tests preserve license

--- bin/image/tests.js
+++ bin/image/tests.js
@@ -48,3 +48,26 @@ ava.serial('image resolver prefer server', async t => {
     t.true(description.edition === 'server');
     t.true(description.distro === 'ubuntu');
 });
+
+ava.serial('image preserve license', async t => {
+    const disk_name = tests.getName('disk', t.title);
+    // create vm from recommended image
+    const vm_recommended = await tests.run(`vm create --type a1.nano --name ${tests.getName('vm recommended', t.title)} --image rhel --os-disk ${disk_name},ssd,40 --no-start`);
+    const vm_services = await tests.run(`vm service list --vm ${vm_recommended.id}`);
+    t.true(vm_services.some(x => x.type == 'license'));
+    // create image of vm
+    const image = await tests.run(`image create --name ${tests.getName('image', 1, t.title)} --vm ${vm_recommended.id}`);
+    t.true(image.license && image.license.length > 0);
+    // clean up vm from recommended image
+    await tests.remove('vm', vm_recommended);
+    await tests.remove('disk', disk_name);
+    // create vm from user image (with license)
+    const vm_user = await tests.run(`vm create --type a1.nano --name ${tests.getName('vm user', t.title)} --image ${image.id} --os-disk ${disk_name},ssd,40 --no-start`);
+    const vm_user_services = await tests.run(`vm service list --vm ${vm_user.id}`);
+    t.true(vm_user_services.some(x => x.type == 'license'));
+    // clean up vm from user image
+    await tests.remove('image', image);
+    // clean up vm from user image
+    await tests.remove('vm', vm_user);
+    await tests.remove('disk', disk_name);
+});
