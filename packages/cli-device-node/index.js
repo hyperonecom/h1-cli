b69a39a797f97c994ce3f76c013ea045acb92586
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-09-09 17:21:40 +0200

    cli-device-node: Support as commonjs2 package

---
+++ packages/cli-device-node/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+
+import { buildCli } from '@hyperone/cli-core';
+import { NodeDevice } from './device';
+
+export {
+    NodeDevice,
+    buildCli,
+};
--- packages/cli-device-node/webpack.config.js
+++ packages/cli-device-node/webpack.config.js
@@ -5,11 +5,13 @@ module.exports = {
     entry: {
         h1: './bin/h1.js',
         h1_docs: './scripts/print-docs.js',
+        main: './index.js',
         // rbx: './bin/rbx.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+        libraryTarget: 'commonjs2',
     },
     target: 'node',
     mode: process.env.NODE_ENV || 'development',
