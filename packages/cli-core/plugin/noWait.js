43ddc9e38fe12f35b3ffc3a01139da7b8859bf17
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-09-18 17:53:51 +0200

    Fix no-wait parameter

--- packages/cli-core/plugin/noWait.js
+++ packages/cli-core/plugin/noWait.js
@@ -12,7 +12,7 @@ export default {
     ),
     beforeCommandStart: async (opts) => {
         const optsAll = opts._all || opts;
-        const prefer_async = optsAll.noWait;
+        const prefer_async = optsAll['no-wait'];
         const prefer_value = prefer_async ? 'respond-async,wait=0' : `respond-async,wait=${60 * 60 * 24}`;
         opts.api.setHeaders({
             Prefer: prefer_value,
