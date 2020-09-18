fe117ad6df98784831355ecd1a4fe8dcc0a6c0d7
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-09-18 18:38:08 +0200

    Fix no-wait support

--- packages/cli-core/plugin/noWait.js
+++ packages/cli-core/plugin/noWait.js
@@ -4,16 +4,18 @@ export default {
     beforeParseArgv: (cmd) => cmd.options.push(
         {
             name: 'no-wait',
-            type: Boolean,
-            description: 'In case of queued event do not wait for completion',
+            type: (value) => value == 'true',
+            typeLabel: 'true,false',
+            choices: ['true', 'false'],
             defaultValue: true,
+            description: 'In case of queued event do not wait for completion',
             group: ['global'],
         }
     ),
     beforeCommandStart: async (opts) => {
         const optsAll = opts._all || opts;
         const prefer_async = optsAll['no-wait'];
-        const prefer_value = prefer_async ? 'respond-async,wait=0' : `respond-async,wait=${60 * 60 * 24}`;
+        const prefer_value = prefer_async ? `respond-async,wait=${60 * 60 * 24}` : 'respond-async,wait=0';
         opts.api.setHeaders({
             Prefer: prefer_value,
         });
