76307f7a8e6cce8d3f2b8ead2d15573cb72126d0
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-12-29 12:44:32 +0100

    Add summary to version command

--- extensions/cli-ext-root-version/commands/version.js
+++ extensions/cli-ext-root-version/commands/version.js
@@ -2,8 +2,7 @@ import { Command } from '@hyperone/cli-framework';
 
 export default new Command({
     name: 'version',
-    options: [
-    ],
+    summary: 'Display CLI version and exit',
     handler: async (opts, cmd) => {
         return cmd.device.getVersion();
     },
