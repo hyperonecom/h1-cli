86a03705b4c54c811688b09c5fe83e50aade4fb0
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-12-30 05:17:40 +0100

    Skip tests with authentication

--- packages/cli-device-node/tests/auth.js
+++ packages/cli-device-node/tests/auth.js
@@ -1,12 +1,12 @@
 const ava = require('ava');
 const { run, runJson, withTemp, withVariable } = require('./../lib/tests');
 
-ava('display user profile', async t => {
+ava.skip('display user profile', async t => {
     const output = await runJson('h1 auth me');
     t.true(output.aud.includes('https://api.hyperone.com/v2'));
 });
 
-ava('auth user', withTemp(withVariable(['username', 'password'], async (t, tmpDir, username, password) => {
+ava.skip('auth user', withTemp(withVariable(['username', 'password'], async (t, tmpDir, username, password) => {
     const options = {env: {HOME: tmpDir}};
     const output = await run(`h1 auth user --username ${username} --password ${password}`, options);
     t.true(output.includes('Token successfully updated.'));
@@ -14,7 +14,7 @@ ava('auth user', withTemp(withVariable(['username', 'password'], async (t, tmpDi
     t.true(me.sub.includes(username));
 })));
 
-ava('auth aws', withTemp(withVariable(['aws-access-key', 'aws-secret-key'], async (t, tmpDir, access_key, secret_key) => {
+ava.skip('auth aws', withTemp(withVariable(['aws-access-key', 'aws-secret-key'], async (t, tmpDir, access_key, secret_key) => {
     const options = {env: {HOME: tmpDir}};
     const output = await run(`h1 auth aws --access-key-id ${access_key} --secret-access-key ${secret_key}`, options);
     t.true(output.includes('Token successfully updated.'));
