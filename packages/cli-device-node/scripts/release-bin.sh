c1af90323f912b4e48ddf96dcb9c1b79d85ac9be
Frederico Silva <frederico.silva@gmail.com>
2022-11-14 11:57:10 +0000

    cli-device-node: upgrade to node 18

--- packages/cli-device-node/scripts/release-bin.sh
+++ packages/cli-device-node/scripts/release-bin.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -eux
-NODE_VERSION="12"
+NODE_VERSION="18"
 
 for scope in h1; do
-	npx pkg -t "node${NODE_VERSION}-linux" -o "./dist/${scope}" "./dist/${scope}.js";
+	npx pkg -t "node${NODE_VERSION}-linuxstatic" -o "./dist/${scope}" "./dist/${scope}.js";
 	tar czf ./dist/${scope}-linux.tar.gz -C ./dist/ ${scope}
 	rm ./dist/${scope}
 	
@@ -14,8 +14,4 @@ for scope in h1; do
 	npx pkg -t "node${NODE_VERSION}-win" -o "./dist/${scope}.exe" "./dist/${scope}.js";
 	zip -j ./dist/${scope}-win.zip ./dist/${scope}.exe
 	rm ./dist/${scope}.exe
-
-	docker run -v $(pwd):/src -w /src "node:${NODE_VERSION}-alpine" npx pkg -t "node${NODE_VERSION}-alpine" -o "./dist/${scope}" "./dist/${scope}.js";
-	tar czf ./dist/${scope}-alpine.tar.gz -C ./dist/ ${scope}
-	docker run -v $(pwd):/src -w /src "node:${NODE_VERSION}-alpine" npx rm ./dist/${scope}
 done;
