69a57275fd5f94a99bdfc1743b7e06c8e9856b33
Frederico Silva <frederico.silva@gmail.com>
2023-02-15 17:02:01 +0000

    openapi: add format password-unix support

--- extensions/cli-ext-root-openapi-generator/middlewares/index.js
+++ extensions/cli-ext-root-openapi-generator/middlewares/index.js
@@ -1,7 +1,9 @@
+import password from './password-unix.js';
 import upload from './uri-upload.js';
 
 const formats = {
     uri: upload,
+    'password-unix': password,
 };
 
 export default formats;
---
+++ extensions/cli-ext-root-openapi-generator/middlewares/password-unix.js
@@ -0,0 +1,24 @@
+import { sha512crypt } from 'sha512crypt-node';
+import { set } from '@hyperone/cli-core/lib/transform.js';
+
+export default {
+    name: 'password-unix',
+    beforeRequest: async (requestBody, requestUrl, opts, options, cmd) => {
+        const optsAll = opts._all || opts;
+
+        for (const option of options) {
+            const value = optsAll[option.name];
+            if (!value) {
+                continue;
+            }
+
+            if (option.use?.schema?.format === 'password-unix') {
+                const salt = Math.random().toString(36).slice(-8);
+                const newValue = sha512crypt(value, salt)
+                set(requestBody, option.use.field.replace(/\//g, '.'), newValue)
+            }
+        }
+
+        return requestBody;
+    },
+};
