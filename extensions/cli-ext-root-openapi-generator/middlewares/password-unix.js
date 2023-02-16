ec820cfcda2e3fbc0c4a1ab6eed3cee107d47dd5
Frederico Silva <frederico.silva@gmail.com>
2023-02-16 14:20:49 +0000

    lint++

--- extensions/cli-ext-root-openapi-generator/middlewares/password-unix.js
+++ extensions/cli-ext-root-openapi-generator/middlewares/password-unix.js
@@ -3,7 +3,7 @@ import { set } from '@hyperone/cli-core/lib/transform.js';
 
 export default {
     name: 'password-unix',
-    beforeRequest: async (requestBody, requestUrl, opts, options, cmd) => {
+    beforeRequest: async (requestBody, requestUrl, opts, options) => {
         const optsAll = opts._all || opts;
 
         for (const option of options) {
@@ -14,8 +14,8 @@ export default {
 
             if (option.use?.schema?.format === 'password-unix') {
                 const salt = Math.random().toString(36).slice(-8);
-                const newValue = sha512crypt(value, salt)
-                set(requestBody, option.use.field.replace(/\//g, '.'), newValue)
+                const newValue = sha512crypt(value, salt);
+                set(requestBody, option.use.field.replace(/\//g, '.'), newValue);
             }
         }
 
