0e0269f87470fedeb6348dd322a978ef3bdf8291
Frederico Silva <frederico.silva@gmail.com>
2023-02-17 13:05:40 +0000

    cli-ext-storage-bucket: unify injection

--- extensions/cli-ext-storage-bucket/index.js
+++ extensions/cli-ext-storage-bucket/index.js
@@ -5,9 +5,8 @@ import info from './package.json' assert { type: 'json' };
 export default {
     name: info.name,
     version: info.version,
-    load: async (parent) => {
-        parent.loadHook.push(() => {
-            parent.commands.find(c => c.name === 'bucket').addCommand(upload);
-        });
-    },
+    load: async (parent) => parent.loadHook.push(() => {
+        const cmd = parent.commands.find(c => c.name === 'bucket');
+        cmd?.addCommand(upload);
+    }),
 };
